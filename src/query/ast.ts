7a01443ae27992e6e8050a94db1b9c23ec64519d
ezekiel-charo <ezekiel3499@gmail.com>
2026-01-17 18:03:13 +0300

    Add types for table CRUD abstract syntax trees

---
+++ src/query/ast.ts
@@ -0,0 +1,59 @@
+export type ASTNode =
+  | CreateTableAST
+  | InsertAST
+  | SelectAST
+  | UpdateAST
+  | DeleteAST;
+
+export interface CreateTableAST {
+  type: "CREATE_TABLE";
+  tableName: string;
+  columns: {
+    name: string;
+    dataType: "INTEGER" | "TEXT" | "BOOLEAN";
+    isPrimaryKey?: boolean;
+  }[];
+}
+
+export interface InsertAST {
+  type: "INSERT";
+  tableName: string;
+  columns: string[];
+  values: any[][];
+}
+
+export interface SelectAST {
+  type: "SELECT";
+  columns: string[]; // ['*'] or ['name', 'age']
+  from: string;
+  where?: WhereClause;
+  //   join?: JoinClause;
+}
+
+export interface UpdateAST {
+  type: "UPDATE";
+  tableName: string;
+  set: { column: string; value: any }[];
+  where?: WhereClause;
+}
+
+export interface DeleteAST {
+  type: "DELETE";
+  from: string;
+  where?: WhereClause;
+}
+
+export interface WhereClause {
+  column: string;
+  operator: "=" | "!=" | ">" | "<" | ">=" | "<=";
+  value: any;
+}
+
+// export interface JoinClause {
+//   type: "INNER";
+//   table: string;
+//   on: {
+//     leftColumn: string;
+//     rightColumn: string;
+//   };
+// }
